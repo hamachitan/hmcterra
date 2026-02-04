55367ff4c995f41029724c6b0e58e1dede86138d
madomado <madonuko@outlook.com>
2026-02-04 11:49:03 +0800

    perf: hmct cmd regex use lazy

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 export const mdFullPkgNameRegex = /### Full Package Name\n\n(.+)-([^-]+)-([^-\n]+)$/m;
 export const mdRelverRegex = /### Release Version\n\n([\d\w]+)$/m;
-export const commandRegex = /^(@hamachitan|hmct) +(.+) *$/img;
+export const commandRegex = /^(@hamachitan|hmct) +(.+) *?$/img;
 export const HAMACHITAN_USERNAME = 'hamachitan';
 export const MADOGUCHI_BASE_URL = 'https://madoguchi.fyralabs.com';
