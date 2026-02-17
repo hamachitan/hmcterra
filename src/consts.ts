530645e4febecd1751b4e4c04d5e7fc08a594a56
madomado <madonuko@outlook.com>
2026-02-17 11:57:26 +0800

    perf: full package name regex

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const mdFullPkgNameRegex = /### Full Package Name.*\n\n(.+)-([^-]+)-([^-\s]+)/m;
+export const mdFullPkgNameRegex = /### Full Package Name.*?\n\n(.+)-([^-]+)-([^-\s]+)/m;
 export const mdRelverRegex = /### Release Version\n\n([\d\w]+)$/m;
 export const HAMACHITAN_USERNAME = 'hamachitan';
 export const MADOGUCHI_BASE_URL = 'https://madoguchi.fyralabs.com';
