b1adc781f26a19dbc98eb782704ce33be345c8a8
madonuko <madonuko@outlook.com>
2026-05-23 14:21:51 +0800

    fix: update devdocs link

--- src/lints/checkPackager.ts
+++ src/lints/checkPackager.ts
@@ -8,7 +8,7 @@ export async function checkPackager({ specContent, file, app }: LintParams): Pro
     const packager = await runRpmspec(specContent, '%{packager}');
     if (packager === '(none)') {
       return {
-        messages: [`🍣 The \`Packager: name <mail@example.com>\` preamble is missing in \`${file.filename}\`. This is required in our [policies](https://developer.fyralabs.com/terra/policies#packager-field).`],
+        messages: [`🍣 The \`Packager: name <mail@example.com>\` preamble is missing in \`${file.filename}\`. This is required in our [policies](https://docs.terrapkg.com/contributing/policies/#packager-field).`],
         reviewComments: []
       };
     }
